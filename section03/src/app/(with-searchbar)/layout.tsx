3d1fb17578c0df57fdf177e9ca445784d2cb6794
JunnKyuu <junnkyuu22@gmail.com>
2025-12-25 02:35:42 +0900

    feature: App Router UI 구현

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from "react";
+import Searchbar from "../../components/searchbar";
 
-export default function Layout({ children }: { children: ReactNode }) {
-    return (
-        <div>
-            <div>임시 서치바</div>
-            <div>{children}</div>
-        </div>
-    )
+export default function Layout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <div>
+      <Searchbar />
+      {children}
+    </div>
+  );
 }
