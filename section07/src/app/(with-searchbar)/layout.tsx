8ec357a6a976be4a73f669229629cd487c9b3ee7
JunnKyuu <junnkyuu22@gmail.com>
2025-12-30 09:24:22 +0900

    feature: 서버액션 처리 및 페이지 재검증 구현

---
+++ section07/src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode, Suspense } from "react";
+import Searchbar from "../../components/searchbar";
+
+export default function Layout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <div>
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <Searchbar />
+      </Suspense>
+      {children}
+    </div>
+  );
+}
